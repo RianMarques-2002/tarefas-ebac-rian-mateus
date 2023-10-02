31aea2de228a825a723ce4cdac1eb470eafb347d
Rian Mateus <rianmarquesmateus@gmail.com>
2023-10-02 18:12:05 -0300

    add TarefaWrappers

---
+++ tarefa_wrappers/TarefaWrappers.java
@@ -0,0 +1,12 @@
+package tarefa_wrappers;
+
+public class TarefaWrappers {
+
+    public static void main(String[] args) {
+        int num1 = 15;  // Variável primitiva (int) recebendo valor 15
+        System.out.println(num1);  // Print da variável num1 com o valor 15
+
+        Integer num2 = num1;  //  Conversão da variável num1 com tipo primitivo int para Integer (wrapper)
+        System.out.println(num2);  // Print do tipo complexo Integer com o valor 15
+    }
+}
